e4a0d67237a8c42f1a29a9be71e705344c6820e6
cell2silicon <83265414+cell2silicon@users.noreply.github.com>
2021-09-08 19:07:17 +1000

    Files

--- Flask.py/app.py
+++ Flask.py/app.py
@@ -4,21 +4,22 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+import sqlite3
 
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("sqlite:///life_expectancy.db")
+# #################################################
+# # Database Setup
+# #################################################
+# engine = create_engine("sqlite:///life_expectancy.db")
 # reflect an existing database into a new model
-Base = automap_base()
+# Base = automap_base()
 # reflect the tables
-Base.prepare(engine, reflect=True)
+# Base.prepare(engine, reflect=True)
 # Save reference to the table
-Life_data = Base.classes.life_expectancy
+# Life_data = Base.classes.life_expectancy
 
-#################################################
-# Flask Routes
-#################################################
+# #################################################
+# # Flask Routes
+# #################################################
 
 app = Flask(__name__)
 
@@ -30,28 +31,43 @@ def welcome():
         f"/api/v1.0/life_expectancy<br/>"
         
     )
-@app.route("/api/v1.0/life_expectancy")
-def names():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-    """Return a list of all passenger names"""
+# @app.route("/api/v1.0/life_expectancy")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+#     """Return a list of all passenger names"""
     # Query all passengers
-    results = session.query(Life_data.name).all()
-    session.close()
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-    return jsonify(all_names)
+#     results = session.query(life.name).all()
+#     session.close()
+#     # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
+#     return jsonify(all_names)
 
 
-# Create a dictionary from the row data and append to a list of all_passengers
-    # all_passengers = []
-    # for name, age, sex in results:
-    #     passenger_dict = {}
-    #     passenger_dict["name"] = name
-    #     passenger_dict["age"] = age
-    #     passenger_dict["sex"] = sex
-    #     all_passengers.append(passenger_dict)
-    # return jsonify(all_passengers)
+# # Create a dictionary from the row data and append to a list of all_passengers
+#     # all_passengers = []
+#     # for name, age, sex in results:
+#     #     passenger_dict = {}
+#     #     passenger_dict["name"] = name
+#     #     passenger_dict["age"] = age
+#     #     passenger_dict["sex"] = sex
+#     #     all_passengers.append(passenger_dict)
+#     # return jsonify(all_passengers)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# 
+# @app.route('/list')
+# def list():
+#    con = sqlite3.connect("life_expectancy.db")
+#    con.row_factory = sqlite3.Row
+   
+#    cur = con.cursor()
+#    cur.execute("select * from life")
+   
+#    rows = cur.fetchall();
+#    return render_template("list.html",rows = rows)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug = True)
